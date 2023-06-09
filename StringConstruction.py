a5c41b972d9a4efc536cea3edae0222532237e0d
Samarth Gupta <samarthgupta@Samarths-MacBook-Pro.local>
2023-06-09 14:50:03 -0400

    hackerrank 9 june

---
+++ StringConstruction.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stringConstruction' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def stringConstruction(s):
+    # Write your code here
+    dict = {}
+    for c in s:
+        if c not in dict:
+            dict[c] = 1
+    cnt = 0
+    for x in dict:
+        cnt+=1
+        
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = stringConstruction(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
