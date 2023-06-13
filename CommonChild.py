9d9f710793ff17798eea4732e40634a70cbcb830
Samarth Gupta <samarthgupta@Samarths-MacBook-Pro.local>
2023-06-13 14:49:30 -0400

    13 june solved hackerrank questions

---
+++ CommonChild.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'commonChild' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+def commonChild(s1, s2):
+    m = len(s1)
+    n = len(s2)
+    dp = [[None]*(n + 1) for i in range(m + 1)]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0 :
+                dp[i][j] = 0
+            elif s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+ 
+    return dp[m][n]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+
+    s2 = input()
+
+    result = commonChild(s1, s2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
